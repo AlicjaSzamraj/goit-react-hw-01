43bdd029102a7bb186201cb22d3b8b534050882b
AlicjaSzamraj <120415960+AlicjaSzamraj@users.noreply.github.com>
2024-10-30 17:47:49 +0000

    profile and app updated

---
+++ goit-react-hw-01/src/Profile.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Profile = ({ name, tag, location, image, stats }) => {
+  return (
+    <div className="profile">
+      <div>
+        <img
+          src={image || 'https://cdn-icons-png.flaticon.com/512/1077/1077012.png'}
+          alt="User avatar"
+        />
+        <p>{name}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </div>
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
