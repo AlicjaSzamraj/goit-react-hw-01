96549992e328d093824984ffe4f9557f537fc88b
AlicjaSzamraj <120415960+AlicjaSzamraj@users.noreply.github.com>
2024-10-30 17:52:04 +0000

    added userData.json

--- goit-react-hw-01/src/App.jsx
+++ goit-react-hw-01/src/App.jsx
@@ -1,31 +1,19 @@
-import React from 'react';
-import Profile from './Profile';
+import React from "react";
+import Profile from "./Profile";
+import userData from "./userData.json";
 
-const userData = {
-    username: "Jacques Gluke",
-    tag: "jgluke",
-    location: "Ocho Rios, Jamaica",
-    avatar: "<https://cdn-icons-png.flaticon.com/512/2922/2922506.png>",
-    stats: {
-      followers: 5603,
-      views: 4827,
-      likes: 1308
-    }
-  };
-  
-  const App = () => {
-    return (
-      <>
-        <Profile
-          name={userData.username}
-          tag={userData.tag}
-          location={userData.location}
-          image={userData.avatar}
-          stats={userData.stats}
-        />
-      </>
-    );
-  };
-  
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
 
 export default App;
