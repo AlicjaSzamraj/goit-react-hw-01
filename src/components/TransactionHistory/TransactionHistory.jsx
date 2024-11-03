c2c82a8b43d66b88d9ff968efde24f38a1a2aae7
AlicjaSzamraj <120415960+AlicjaSzamraj@users.noreply.github.com>
2024-11-03 20:43:50 +0000

    vercel fixed

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -6,20 +6,20 @@ const capitalizeFirstLetter = (string) => {
 };
 const TransactionHistory = ({ items }) => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+    <table className={styles.table}>
+      <thead className={styles.thead}>
+        <tr className={styles.tr}>
+          <th className={styles.th}>Type</th>
+          <th className={styles.th}>Amount</th>
+          <th className={styles.th}>Currency</th>
         </tr>
       </thead>
-      <tbody>
+      <tbody className={styles.tbody}>
         {items.map(({ id, type, amount, currency }) => (
-          <tr key={id}>
-            <td>{capitalizeFirstLetter(type)}</td>
-            <td>{amount}</td>
-            <td>{currency}</td>
+          <tr className={styles.tr} key={id}>
+            <td className={styles.td}>{capitalizeFirstLetter(type)}</td>
+            <td className={styles.td}>{amount}</td>
+            <td className={styles.td}>{currency}</td>
           </tr>
         ))}
       </tbody>
